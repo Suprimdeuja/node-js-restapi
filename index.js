ca39bcbe676fc613a65df588cf85dfe61b9739b1
Suprim Deuja <deujasuprim@gmail.com>
2025-12-27 09:28:17 +0545

    getUser data

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 let express = require("express");
+let data = require("./MOCK_DATA.json")
 let app = express();
 let PORT = 8500;
 
+app.get("/api/userData",(req,res)=>{
+   return res.json(data)
+})
 
-
-
+app.get("/api/userData/:idName",(req,res)=>{
+   let userId = req.params.idName;
+   let result= data.find(userEle => userEle.id === userId)
+   return res.json(result)
+})
 
 app.listen(PORT,()=>{
     console.log("Server has been started")
