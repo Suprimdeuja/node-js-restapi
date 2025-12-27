afaa9ac0dcba91b60e7eb3d87e6203dba060405d
Suprim Deuja <deujasuprim@gmail.com>
2025-12-27 12:21:04 +0545

    userData

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ let data = require("./MOCK_DATA.json")
 let app = express();
 let PORT = 8500;
 
+//userData in ul form
+app.get("/userData",(req,res)=>{
+   let result =data.map(userData => `<ul><li>${userData.first_name}</li></ul>`).join("")
+   return res.send(result)
+})
+
 app.get("/api/userData",(req,res)=>{
    return res.json(data)
 })
 
-app.get("/api/userData/:idName",(req,res)=>{
+app.get("/userData/:idName",(req,res)=>{
    let userId = req.params.idName;
    let result= data.find(userEle => userEle.id === userId)
    return res.json(result)
